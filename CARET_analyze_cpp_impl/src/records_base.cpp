97d2d812f5cba2b43495f9c8927d4fb1e83d0ec8
rokamu623 <r.okamura.061@ms.saitama-u.ac.jp>
2023-05-24 17:56:06 +0900

    temporary fix
    Signed-off-by: rokamu623 <r.okamura.061@ms.saitama-u.ac.jp>

--- CARET_analyze_cpp_impl/src/records_base.cpp
+++ CARET_analyze_cpp_impl/src/records_base.cpp
@@ -716,7 +716,9 @@ std::unique_ptr<RecordsBase> RecordsBase::merge_sequential_for_addr_track(
         [&stamp_sets, &source_key, &record, &column_timestamp](const Record & x) {
           auto timestamp = x.get(column_timestamp);
           std::shared_ptr<StampSet> stamp_set = stamp_sets[timestamp];
-          bool has_same_source_addrs = stamp_set->count(record.get(source_key)) > 0;
+          bool has_same_source_addrs = false;
+          if(stamp_set != nullptr)
+            has_same_source_addrs = stamp_set->count(record.get(source_key)) > 0;
           return has_same_source_addrs;
         };
       std::vector<uint64_t> merged_addrs;
